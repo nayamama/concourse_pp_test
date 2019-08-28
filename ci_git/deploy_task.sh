faaeef9803d64610e4bd8ace0bad1dc9b2f51a9d
Qi Yang <nayamama@hotmail.com>
2019-08-28 00:09:29 -0400

    change image_resource type

--- ci_git/deploy_task.sh
+++ ci_git/deploy_task.sh
@@ -2,7 +2,6 @@
 
 set -x
 
-<<<<<<< HEAD
 echo $PWD
 echo "script starts running"
 
@@ -10,9 +9,5 @@ touch private_key.pem
 
 echo "touch the file"
 
-=======
-touch private_key.pem
-
->>>>>>> c4c12e7e67e611fd86e58568d5e9a9c019a0a29e
 echo $PRIVATE_KEY > private_key.pem
 cat private_key
