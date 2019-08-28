38653ac30bc770812378226706eec42d4294987f
Qi Yang <nayamama@hotmail.com>
2019-08-28 00:59:45 -0400

    test

--- ci_git/deploy_task.sh
+++ ci_git/deploy_task.sh
@@ -13,9 +13,9 @@ sudo chmod 600 private_key.pem
 
 echo "touch the file"
 
-echo $PRIVATE_KEY > private_key.pem
-#echo $PRIVATE_KEY | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > private_key.pem
+#echo $PRIVATE_KEY > private_key.pem
+echo $PRIVATE_KEY | sed -e 's/\(KEY-----\)\s/\1\n/g; s/\s\(-----END\)/\n\1/g' | sed -e '2s/\s\+/\n/g' > private_key.pem
 cat private_key.pem
 
-#file private_key.pem
+file private_key.pem
 scp -oStrictHostKeyChecking=no -i private_key.pem private_key.pem root@shilei.mooo.com:/root
