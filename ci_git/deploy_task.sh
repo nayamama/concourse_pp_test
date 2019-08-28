b99d25ac2102d52a3c1627d430ccb3fe2691d8bb
Qi Yang <nayamama@hotmail.com>
2019-08-28 00:33:42 -0400

    try to ssh to ali cloud

--- ci_git/deploy_task.sh
+++ ci_git/deploy_task.sh
@@ -12,4 +12,5 @@ echo "touch the file"
 echo $PRIVATE_KEY > private_key.pem
 cat private_key.pem
 
-file private_key.pem
+#file private_key.pem
+scp -oStrictHostKeyChecking=no -i private_key.pem private_key.pem root@shilei.mooo.com:/root
